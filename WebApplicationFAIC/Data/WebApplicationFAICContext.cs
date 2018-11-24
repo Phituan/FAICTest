6cc4c344aef0f10f429d2c50c01d79a62f8de110
ok <tuandpd00617@fpt.edu.vn>
2018-11-24 10:25:52 +0700

    Ok

---
+++ WebApplicationFAIC/Data/WebApplicationFAICContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationFAIC.Models;
+
+namespace WebApplicationFAIC.Models
+{
+    public class WebApplicationFAICContext : DbContext
+    {
+        public WebApplicationFAICContext (DbContextOptions<WebApplicationFAICContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplicationFAIC.Models.Category> Category { get; set; }
+
+        public DbSet<WebApplicationFAIC.Models.Product> Product { get; set; }
+    }
+}
